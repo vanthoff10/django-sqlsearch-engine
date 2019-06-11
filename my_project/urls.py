d55f9d804f02cfc9e6ea1ccc07a1d23ad7b4ce71
Ashwin <ashwinpanwar1019999@gmail.com>
2019-06-11 16:01:38 +0530

    before sql commmit final

--- my_project/urls.py
+++ my_project/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
+from users import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('users/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('contact', views.create)
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
+
+def index(request):
+    return render(request)
+
+
+def create(request):
+    print(request.method)
+    if request.method == "POST":
+        input_text = request.POST['search']
+        print(input_text)
+        print('*'*50)
+        return HttpResponse("welcome")
